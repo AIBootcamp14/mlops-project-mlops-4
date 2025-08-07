2dc7121c2be144efa360cd2599afb27627ec5ea4
Ubuntu <ubuntu@ip-172-31-34-9.ap-northeast-2.compute.internal>
2025-08-07 21:00:23 +0000

    fix: starter files are now connected to correct address

--- mlops_proj/start_api_server.sh
+++ mlops_proj/start_api_server.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-nohup python src/webapp.py &
+
+if command -v python &>/dev/null; then
+    nohup python src/webapp.py &
+elif command -v python3 &>/dev/null; then
+    nohup python3 src/webapp.py &
+else
+    echo "Python is not installed."
+    exit 1
+fi
+
